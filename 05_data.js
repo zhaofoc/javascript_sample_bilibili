80fdc2769b8caa58736ba2d94d3f39a6a46563ab
zhaofoc <122864880+zhaofoc@users.noreply.github.com>
2023-03-02 15:07:28 +0800

    Create 05_data.js

---
+++ 05_data.js
@@ -0,0 +1,59 @@
+//This is sample code for video: https://www.bilibili.com/video/BV1QP4y167Aj
+//数据类型
+
+//let
+
+let sample = 10;
+console.log(sample);
+
+sample = 20;
+console.log(sample);
+
+//const
+
+const sampleTwo = 10;
+console.log(sampleTwo);
+
+//sampleTwo = 20;
+//console.log(sampleTwo);
+
+console.log(typeof sample);
+
+sample = 114.514;
+
+console.log(typeof sample);
+
+sample = -114.514;
+
+console.log(typeof sample);
+
+//number
+
+sample = "iiyo koiyo";
+
+console.log(sample);
+console.log(typeof sample);
+
+//string
+
+sample = true;
+console.log(sample);
+console.log(typeof sample);
+
+sample = false;
+console.log(sample);
+console.log(typeof sample);
+
+//boolean
+
+sample = null;
+console.log(sample);
+console.log(typeof sample);
+
+//null null(object)
+
+sample = undefined;
+console.log(sample);
+console.log(typeof sample);
+
+//undefined undefined
